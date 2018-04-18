b394bab1e5d7d38869fec31b35a8a8c422232093
Mike Stewart <michael.stewart@uibk.ac.at>
2018-04-18 18:19:06 +0200

    Karten eingefügt

--- basemap.js
+++ basemap.js
@@ -1,1 +1,22 @@
-aleret("Hallo Welt!");
+
+let myMap = L.map("mapdiv");
+let myLayers = {
+    
+    osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+
+    geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
+    }
+),
+
+    bmapoverlay : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"),
+
+    bmapgrau : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"),
+
+    bmaphidpi : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"),
+
+    bmaporthofoto30cm : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"),
+};
+myMap.addLayer(myLayers.geolandbasemap);
+myMap.setView([47.267,11.383], 11);
