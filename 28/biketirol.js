cd8abb65b29408321bd3d730df684288c1205cf8
Mike Stewart <michael.stewart@uibk.ac.at>
2018-05-22 13:19:03 +0200

    Biketrail Tirol funktioniert jetz

--- 28/biketirol.js
+++ 28/biketirol.js
@@ -1,126 +1,137 @@
-
 let myMap = L.map("map", {
-    fullscreencontrol: true
-});    
-let bikeGroup = L.featureGroup().addTo(myMap)
-
+    fullscreenControl: true
+});
+let bikeGroup = L.featureGroup().addTo(myMap);
+let bikeLine = L.featureGroup().addTo(myMap);
 let myLayers = {
-    
-    osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {    //http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
-        subdomains : ["a","b","c"],
-        attribution : "Datenquelle: <a href='https://www.openstreetmap.org'> openstreetmap.at </a>"
-    }
-),
-    geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains : ["maps","maps1","maps2","maps3","maps4"],  //http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
-        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"    //http://leafletjs.com/reference-1.3.0.html#layer-attribution
-    }
-),
-    summer : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
-    }
+    osm : L.tileLayer (
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            subdomains : ["a","b","c"], 
+            attribution : "Datenquelle: <a href='https://www.openstreetmap.org'> openstreetmap.at </a>"
+        }
+    ),
+    geolandbasemap : L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    bmapoverlay :  L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"],
+            attribution : "Datenquelle: <a href='https://www.basemap.at'> basemap.at </a>"
+        }
+    ),
+    Summer : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
 ), 
-    winter : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
-    }
+	Winter : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
 ), 
-    ortho : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
-    }
-),
+	Ortho : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
+), 
 };
 
-myMap.addLayer(myLayers.geolandbasemap);    //http://leafletjs.com/reference-1.3.0.html#layer-onadd
 
-let myMapControl = L.control.layers({   //http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
+
+
+myMap.addLayer(myLayers.geolandbasemap);
+let myMapControl = L.control.layers({ 
     "Openstreetmap" : myLayers.osm,
     "Geolandbasemap" : myLayers.geolandbasemap,
-    "Sommerkarte": myLayers.summer,
-    "Winterkarte": myLayers.winter,
-    "Orthokarte": myLayers.ortho,
+    "Sommerkarte": myLayers.Summer,
+    "Winterkarte": myLayers.Winter,
+    "Orthokarte": myLayers.Ortho,
 
-},{
-    "Bmapoverlay" : myLayers.bmapoverlay,
-    "Route": bikeGroup,
 }, {
-    collapsed : false       //http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
+    "Bmapoverlay" : myLayers.bmapoverlay,
+    "Stationen" : bikeGroup,
+    "Route": bikeLine,
+}, {
+    collapsed : false
 });
 
-myMap.addControl(myMapControl);     //http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+myMap.addControl(myMapControl);
 
-myMap.setView([47.267,11.383], 11); //http://leafletjs.com/reference-1.3.0.html#map-setview
+L.control.scale({
+    position: "bottomleft",
+    maxWidth: 200,
+    metric: true,
+    imperial: false,
+    }
+).addTo(myMap);
 
-L.control.scale({                    //http://leafletjs.com/reference-1.3.0.html#control-scale-l-control-scale
-    position: 'bottomleft',         //http://leafletjs.com/reference-1.3.0.html#control-scale-position
-    maxWidth: 200,                  //http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
-    metric: true,                   //http://leafletjs.com/reference-1.3.0.html#control-scale-metric
-    imperial: false,                //http://leafletjs.com/reference-1.3.0.html#control-scale-imperial
-    }).addTo(myMap);
+const wenns = [47.167234,10.729962];
+const landeck = [47.136962,10.566817]
 
-const wenns = [47.167234, 10.729962];
-const landeck = [47.136962, 10.566817]
 const iconStart = L.icon({
-    iconUrl: "images/start.png"
+    iconUrl: "data/startfinish.png",
+    iconAnchor : [16,37],
+    popupAnchor : [0,-37],
 });
-const iconFinish= L.icon({
-    iconUrl: "images/finish.png"
+const iconFinish = L.icon({
+    iconUrl: "data/finish.png",
+    iconAnchor : [16,37],
+    popupAnchor : [0,-37],
 });
 
 const markerOptions1 = {
-    title: "Wenns - Landeck",
-    opactiy: 1,
-    dragable: false,
+    title: "Wenns - Landeck", // feste Optionen für alle
+    opacity: 1,
+    draggable: false,
     icon: iconStart,
 };
 const markerOptions2 = {
-    title: "Wenns - Landeck",
-    opactiy: 1,
-    dragable: false,
+    title: "Wenns - Landeck", // feste Optionen für alle
+    opacity: 1,
+    draggable: false,
     icon: iconFinish,
 };
-
-L.marker(wenns,markerOptions1).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Wenns'>Wenns im Pitztal</a></p>")
-L.marker(landeck,markerOptions2).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Landeck_(Tirol)'>Landeck</a></p>");
-
-  myMap.fitBounds(bikeGroup.getBounds());
-  
+L.marker(wenns, markerOptions1).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Wenns'><strong>Wenns</strong></a></p><img style = 'width:200px' src='https://www.pitztal.com/sites/default/files/styles/adaptive/public/img_8040.jpg?itok=A0_-oVx5' alt='Wenns' />");
+L.marker(landeck, markerOptions2).addTo(bikeGroup).bindPopup("<p><a href='https://de.wikipedia.org/wiki/Landeck_(Tirol)'><strong>Landeck</strong></a></p><img style = 'width:200px' src='https://thumbnails.trvl-media.com/q2l-vkZO3LO7QjrLyXmQasp3Evk=/768x432/images.trvl-media.com/media/content/shared/images/travelguides/destination/6046522/Seefeld-In-Tirol-48290.jpg' alt='Landeck' />");
 
 
-const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&srsName=EPSG:4326&outputFormat=json&typeName=ogdwien:SPAZIERPUNKTOGD,ogdwien:SPAZIERLINIEOGD"
+
+//Hilfe fuer leaflet.gpx: https://github.com/mpetazzoni/leaflet-gpx
+let gpxTrack = new L.GPX("data/etappe28.gpx", {
+    async: true,
+}).addTo(bikeLine);
+gpxTrack.on("loaded", function(evt) {
+    //console.log(evt.target.get_distance().toFixed(0));
+    //console.log(evt.target.get_elevation_min());
+    let laenge = evt.target.get_distance().toFixed(0);
+    document.getElementById("laenge").innerHTML = laenge;
+
+    myMap.fitBounds(evt.target.getBounds());
+});
 
 
- myMap.addLayer(bikeGroup);
 
- /*let geojson = L.geoJSON(toGeoJSON).addTo(bikeGroup);
+//myMap.fitBounds(bikeGroup.getBounds());
+
+/*let geojson = L.geoJSON(toGeoJSON).addTo(bikeLine);
 geojson.bindPopup(function(layer) {
     //console.log("Layer for Popup:", layer.feature.geometry); 
     const props = layer.feature.geometry;
     const line = `<p>${props.coordinates}</p>`;
     return line // popupText;
 });*/
-
-let gpxTrack = new L.GPX("data/etappe28.gpx", {
-    async : true,
-}).addTo(bikeGroup);
-gpxTrack.on("loaded", function(evt) {
-    console.log("get_distance",evt.target.get_distance().toFixed(0))
-    console.log("get_elevation_min",evt.target.get_elevation_min().toFixed(0))
-    console.log("get_elevation_max",evt.target.get_elevation_max().toFixed(0))
-    console.log("get_elevation_gain",evt.target.get_elevation_gain().toFixed(0))
-    console.log("get_elevation_loss",evt.target.get_elevation_loss().toFixed(0))
-    let laenge = evt.target.get_distance().toFixed(0);
-    document.getElementById("laenge").innerHTML = laenge;
-    myMap.fitBounds(evt.target.getBounds());
-});
-
 // eine neue Leaflet Karte definieren
 
 // Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
 // WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
+
+// Maßstab metrisch ohne inch
+
+// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
 
 // GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
 // Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen*/
