70885def1b00d7f5a035b99a129d1400bd461846
Mike Stewart <michael.stewart@uibk.ac.at>
2018-05-22 13:38:37 +0200

    Marker verschoben und unnötige Dateien gelöscht

--- 28/biketirol.js
+++ 28/biketirol.js
@@ -66,15 +66,15 @@ L.control.scale({
 ).addTo(myMap);
 
 const wenns = [47.167234,10.729962];
-const landeck = [47.136962,10.566817]
+const landeck = [47.136962, 10.566817]
 
 const iconStart = L.icon({
-    iconUrl: "data/startfinish.png",
+    iconUrl: "images/startfinish.png",
     iconAnchor : [16,37],
     popupAnchor : [0,-37],
 });
 const iconFinish = L.icon({
-    iconUrl: "data/finish.png",
+    iconUrl: "images/finish.png",
     iconAnchor : [16,37],
     popupAnchor : [0,-37],
 });
@@ -105,6 +105,14 @@ gpxTrack.on("loaded", function(evt) {
     //console.log(evt.target.get_elevation_min());
     let laenge = evt.target.get_distance().toFixed(0);
     document.getElementById("laenge").innerHTML = laenge;
+    let tiefsterP = evt.target.get_elevation_min().toFixed(0);
+    document.getElementById("tiefsterP").innerHTML = tiefsterP;
+    let hoechsterP = evt.target.get_elevation_max().toFixed(0);
+    document.getElementById("hoechsterP").innerHTML = hoechsterP;
+    let aufstieg = evt.target.get_elevation_gain().toFixed(0)
+    document.getElementById("aufstieg").innerHTML = aufstieg;
+    let abstieg = evt.target.get_elevation_loss().toFixed(0)
+    document.getElementById("abstieg").innerHTML = abstieg;
 
     myMap.fitBounds(evt.target.getBounds());
 });
